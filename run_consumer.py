ae62a2333ddb4ae3c151fcaf3052b674a26d2d6a
hadyelfadaly <hadyelfadaly@gmail.com>
2026-09-22 16:22:30 +0300

    Added prodcuer and consumer into docker compose

--- run_consumer.py
+++ run_consumer.py
@@ -1,10 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 import redis
 from src.workers.worker_runtime import create_integrated_worker
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=0)
 worker = create_integrated_worker(redis_client)
 
 print("Consumer started, waiting for incidents...")
--- run_worker.py
+++ run_worker.py
@@ -1,10 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from src.workers.worker_runtime import create_integrated_worker
 import redis
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=0)
 worker = create_integrated_worker(redis_client)
 celery_app = worker.process_task.app
 
